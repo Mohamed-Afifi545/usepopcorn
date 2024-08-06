1e177c36689efc6b505a58223717f04bb48d4bb9
MOHAMED AFIFI <mada2010545@gmail.com>
2024-08-06 13:02:16 +0300

    finished the star ratings reuseable component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating messages={["Terrible", "bad", "ok", "good", "amazing"]} />
+    <StarRating maxRating={15} />
+    <StarRating
+      maxRating={10}
+      defaultRating={5}
+      color="red"
+      size={30}
+    />
   </React.StrictMode>
 );
